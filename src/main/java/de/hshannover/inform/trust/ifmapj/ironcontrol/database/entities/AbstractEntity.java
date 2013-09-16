7613e9e002116174a13012785eaf50289f5f3dfe
Marcel Reichenbach <MarcelReichenbach@Live.de>
2013-09-16 15:48:05 +0200

    - add new LogCatAppender

---
+++ src/main/java/de/hshannover/inform/trust/ifmapj/ironcontrol/database/entities/AbstractEntity.java
@@ -0,0 +1,37 @@
+package de.hshannover.inform.trust.ifmapj.ironcontrol.database.entities;
+
+import android.database.sqlite.SQLiteDatabase;
+import de.hshannover.inform.trust.ifmapj.ironcontrol.logic.logger.Level;
+import de.hshannover.inform.trust.ifmapj.ironcontrol.logic.logger.Logger;
+import de.hshannover.inform.trust.ifmapj.ironcontrol.logic.logger.LoggerFactory;
+
+/**
+ * @author Marcel Reichenbach
+ * @version 1.0
+ */
+
+public abstract class AbstractEntity {
+
+	private static final Logger logger = LoggerFactory.getLogger(AbstractEntity.class);
+
+	public void onCreate(SQLiteDatabase database) {
+		String tabel = getTable();
+		String createString = getDatabaseCreate();
+
+		logger.log(Level.DEBUG, "Create database(Tabel:"+tabel+") ...");
+		database.execSQL(createString);
+		logger.log(Level.DEBUG, "... Create(Tabel:"+tabel+")");
+	}
+
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		String tabel = getTable();
+		logger.log(Level.DEBUG, "Upgrading database(Tabel:"+tabel+") from version "+ oldVersion + " to " + newVersion + ", destroy all old data ...");
+		database.execSQL("DROP TABLE IF EXISTS " + tabel);
+		onCreate(database);
+		logger.log(Level.DEBUG, "... Upgrading(Tabel:"+tabel+") OK");
+	}
+
+	protected abstract String getTable();
+	protected abstract String getDatabaseCreate();
+
+}
---
+++ src/main/java/de/hshannover/inform/trust/ifmapj/ironcontrol/logic/logger/appander/LogCatAppender.java
@@ -0,0 +1,71 @@
+package de.hshannover.inform.trust.ifmapj.ironcontrol.logic.logger.appander;
+
+import android.util.Log;
+import de.hshannover.inform.trust.ifmapj.ironcontrol.logic.logger.Level;
+
+public class LogCatAppender implements Appender {
+
+	private static final String clientID = "ironcontrol";
+
+	private static final int INITIAL_BUFFER_SIZE = 64;
+
+	@Override
+	public void clear() {
+		// Nothing
+	}
+
+	@Override
+	public void log(String name, long time, Level level, Object message, Throwable t) {
+		switch (level) {
+		case FATAL:
+			Log.wtf(clientID, toString(name, message), t);
+			break;
+
+		case ERROR:
+			Log.e(clientID, toString(name, message), t);
+			break;
+
+		case WARN:
+			Log.w(clientID, toString(name, message));
+			break;
+
+		case INFO:
+			Log.i(clientID, toString(name, message));
+			break;
+
+		case DEBUG:
+			Log.d(clientID, toString(name, message));
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	public String toString(String name, Object message) {
+		StringBuffer buffer = new StringBuffer(INITIAL_BUFFER_SIZE);
+
+		buffer.append('[');
+		buffer.append(Thread.currentThread().getName());
+		buffer.append("] [");
+
+		if(name != null){
+			int index = name.lastIndexOf(".");
+			buffer.append(name.substring(index+1));
+		}
+
+		buffer.append("] ");
+
+		if(message != null){
+			buffer.append(message.toString());
+		}
+
+		return buffer.toString();
+	}
+
+	@Override
+	public long getLogSize() {
+		return -1;
+	}
+
+}
